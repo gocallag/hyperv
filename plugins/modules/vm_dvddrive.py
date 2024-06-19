c55a91d36be745e4c22ef15e20f10d01cb9ac17d
Geoff O'Callaghan <geoffocallaghan@gmail.com>
2024-06-19 17:06:10 +1000

    add molecule test case for vm_dvddrive
    This adds the basic test case for vm_dvddrive

---
+++ plugins/modules/vm_dvddrive.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2024, Geoff O'Callaghan <geoffocallaghan@gmail.com>
+# Re-worked based on best practices at https://docs.ansible.com/ansible/latest/dev_guide/developing_modules_general_windows.html
+
+
+DOCUMENTATION = r'''
+---
+module: vm_dvddrive
+short_description: Manages DVD Drive attached to VM
+description:
+    - Manage the DVD Drive on a Hyper-V Virtual Machine
+options:
+  id:
+    description:
+      - VMId of VM
+    require: true
+  path:
+  state:
+
+'''
+EXAMPLES = r'''
+'''
+
+RETURN = r'''
+pre_cmd:
+  description: the powershell command executed on the Hyper-V Host to perform the pre change check
+  returned: always
+  type: str
+pre_output:
+  description: the raw output of the powershell command executed as part of the pre change check
+  returned: always
+  type: str
+cmd:
+  description: the powershell command executed on the Hyper-V Host
+  returned: always
+  type: str
+output:
+  description: the raw output of the powershell command executed
+  returned: always
+  type: str
+changed:
+  description: if the resource was changed
+  returned: always
+  type: boolean
+'''
+
+EXAMPLES = '''
+'''
